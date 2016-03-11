c7389064c14b2cf9c4fb080a0a1034a0c26a3c22
eespaillat <eespaillat94@gmail.com>
2016-03-10 21:29:11 -0600

    added delete and create portfolio views

--- stockportfolio/api/api.py
+++ stockportfolio/api/api.py
@@ -1,4 +1,5 @@
-from django.http import Http404
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponse
 
 from stockportfolio.api.models import Portfolio, Stock
 from datautils.yahoo_finance import get_current_price, get_company_name
@@ -15,11 +16,21 @@ def remove_stock(request, stock):
     pass
 
 def create_portfolio(request, user_id):
-    # user = User.objects.get()
-    Portfolio.objects.create()
+    user = User.objects.get(pk=user_id)
+    if user is not None:
+        portfolio = Portfolio.objects.create(portfolio_user=user)
+        portfolio.save()
+        raise Http404
+    else:
+        raise HttpResponse(status_code=200)
 
-def delete_portfolio(request):
-    pass
+def delete_portfolio(request, portfolio_id):
+    portfolio = Portfolio.objects.get(portfolio_id)
+    if portfolio is None:
+        raise Http404
+    else:
+        portfolio.delete()
+        return HttpResponse(status_code=200)
 
 def get_portfolio(request):
     pass
