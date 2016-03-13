7c410b245615116c6111de82f038b8f5075e84de
Thibaut Xiong <txiong3@illinois.edu>
2016-03-12 18:51:59 -0600

    refactorings

--- stockportfolio/api/api.py
+++ stockportfolio/api/api.py
@@ -1,11 +1,19 @@
+import json
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponse
-import json
 from stockportfolio.api.models import Portfolio, Stock
 from datautils.yahoo_finance import get_current_price, get_company_name
 
+
 def add_stock(request, portfolio_id):
+    """
+
+    :param request:
+    :param portfolio_id:
+    :return:
+    """
     portfolio = Portfolio.objects.get(portfolio_id=portfolio_id)
+    assert(portfolio is not None)
     stock_ticker = request.GET.get('stock', None)
     if stock_ticker is not None:
         stock_price = get_current_price(stock_ticker)
@@ -13,8 +21,16 @@ def add_stock(request, portfolio_id):
         if stock_name is None or stock_price is None:
             raise Http404
         stock = Stock.objects.create(stock_price=stock_price, stock_name=stock_name, stock_ticker=stock_ticker)
+        assert(stock is not None)
+
 
 def remove_stock(request, portfolio_id):
+    """
+
+    :param request:
+    :param portfolio_id:
+    :return:
+    """
     portfolio = Portfolio.objects.get(portfolio_id=portfolio_id)
     stock_ticker = request.GET.get('stock', None)
     if stock_ticker is not None and portfolio is not None:
@@ -24,7 +40,15 @@ def remove_stock(request, portfolio_id):
             return HttpResponse(status=200)
     return HttpResponse(status=400)
 
+
 def create_portfolio(request, user_id):
+    """
+
+    :param request:
+    :param user_id:
+    :return:
+    """
+    assert(request is not None)
     user = User.objects.get(pk=user_id)
     if user is not None:
         portfolio = Portfolio.objects.create(portfolio_user=user)
@@ -33,7 +57,15 @@ def create_portfolio(request, user_id):
     else:
         raise HttpResponse(status=200)
 
+
 def delete_portfolio(request, portfolio_id):
+    """
+
+    :param request:
+    :param portfolio_id:
+    :return:
+    """
+    assert(request is not None)
     portfolio = Portfolio.objects.get(portfolio_id=portfolio_id)
     if portfolio is None:
         raise Http404
@@ -41,27 +73,30 @@ def delete_portfolio(request, portfolio_id):
         portfolio.delete()
         return HttpResponse(status=200)
 
+
 def get_portfolio(request, portfolio_id):
+    """
+
+    :param request:
+    :param portfolio_id:
+    :return:
+    """
+    assert(request is not None)
     portfolio = Portfolio.objects.get(portfolio_id=portfolio_id)
     if portfolio is None:
         raise Http404
     else:
-        portfolio_dict = {}
-        portfolio_dict["portfolio_id"] = portfolio.portfolio_id
-        portfolio_dict["portfolio_userid"] = portfolio.portfolio_user.pk
-        portfolio_dict["stocks"] = []
-        portfolio_dict["risk_history"] = []
+        portfolio_dict = {'portfolio_id': portfolio.portfolio_id,
+                          'portfolio_userid': portfolio.portfolio_user.pk,
+                          'stocks': [],
+                          'risk_history': []}
         for stock in portfolio.portfolio_stocks.all():
-            stock_dict = {}
-            stock_dict["ticker"] = stock.stock_ticker
-            stock_dict["name"] = stock.stock_name
+            stock_dict = {'ticker': stock.stock_ticker,
+                          'name': stock.stock_name}
             portfolio_dict["stocks"].append(stock_dict)
         for risk in portfolio.portfolio_risk.all().order_by('risk_date'):
-            risk_dict = {}
-            risk_dict["risk_value"] = risk.risk_value
-            risk_dict["risk_date"] = '{}'.format(risk.risk_date)
+            risk_dict = {'risk_valu': risk.risk_value,
+                         'risk_date': '{}'.format(risk.risk_date)}
             portfolio_dict["risk_history"].append(risk_dict)
 
         return HttpResponse(content=json.dumps(portfolio_dict), status=200, content_type='application/json')
-
-
