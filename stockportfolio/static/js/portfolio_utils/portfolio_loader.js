b27d725b2d95f4e5b1df16d34311ef1e3c0077e1
eespaillat <eespaillat94@gmail.com>
2016-03-14 00:46:07 -0500

    espaill2, txiong3, rchakra2: portfolio now being loaded from user.

--- stockportfolio/static/js/portfolio_utils/portfolio_loader.js
+++ stockportfolio/static/js/portfolio_utils/portfolio_loader.js
@@ -1,22 +1,19 @@
-var $url = "api/user/" + $user_id + "/getportfolio";
-raw_json = $.ajax({
+var $url = "/api/user/" + currentUser_id + "/getportfolio";
+console.log($url);
+$.ajax({
     url: $url,
     success: function (data) {
-        populate_table(data);
+        stocks = data.stocks;
+        for (var i = 0; i < stocks.length; i++) {
+            var stock = stocks[i];
+            var $clone = $("table#portfolio").find('tr.hide.sample').clone(true).removeClass("hide")
+            $clone.find("td.company").text(stock.name)
+            $clone.find("td.symbol").text(stock.ticker)
+            $clone.find("td.sector").text(stock.sector)
+            $clone.find("td.quantity").text(stock.quantity)
+            $clone.find("td.last_price").text(stock.price)
+            $clone.find("td.market_value").text(stock.mkt_value)
+            $("table#portfolio").append($clone)
+        }
     }
 });
-
-function populate_table(data) {
-    stocks = data.stocks;
-    for (var i = 0; i < stocks.length; i++) {
-        var stock = stocks[i];
-        var $clone = $("table#portfolio").find('tr.hide.sample').clone(true).removeClass("hide")
-        $clone.find("td.company").text(stock.name)
-        $clone.find("td.symbol").text(stock.ticker)
-        $clone.find("td.sector").text(stock.sector)
-        $clone.find("td.quantity").text(stock.quantity)
-        $clone.find("td.last_price").text(stock.price)
-        $clone.find("td.market_value").text(stock.mkt_value)
-        $("table#portfolio").append($clone)
-    }
-}
