89b1e7825e51bfc64d1c54aa39230c333b092aa9
gleisner <ahashle2@illinois.edu>
2016-03-13 20:44:28 -0500

    gravatar

--- stockportfolio/api/views.py
+++ stockportfolio/api/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse, Http404
 from datautils import yahoo_finance as yf
+from django.template.context_processors import csrf
 from django.contrib.auth.models import User
 from stockportfolio.api.models import Portfolio
-
+import string
+import hashlib
 
 def dashboard(request):
-    #email_hash = str.strip(request.user.email) 
-    context = {"user" : request.user }
+    email = string.lower(string.strip(request.user.email, string.whitespace))
+    g_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email).hexdigest()
+    context = {"user" : request.user, "gravatar": g_url}
+    context.update(csrf(request))
     return render_to_response("index.html", context)
 
 
@@ -15,8 +19,14 @@ def landing(request):
     """Renders the landing page"""
     return render_to_response('landing.html')
 
+# TODO: replace this with a ModelForm
 def profile(request):
-   print request.POST['accountName'] 
+    un = request.POST['accountName']
+    email = request.POST['accountEmail']
+    request.user.username = un;
+    request.user.email = email;
+    request.user.save() 
+    return redirect('dashboard')
 
 def ticker(request, symbol):
     # any api errors bubble up to the user
