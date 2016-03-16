2369f6fbc917c5d5c3de4b072666c9e210405757
eespaillat <eespaillat94@gmail.com>
2016-03-16 15:55:25 -0500

    Moved first ajax call into a global variable

--- stockportfolio/static/js/portfolio_utils/portfolio_loader.js
+++ stockportfolio/static/js/portfolio_utils/portfolio_loader.js
@@ -1,19 +1,12 @@
-var $url = "/api/user/" + currentUser_id + "/getportfolio";
-console.log($url);
-$.ajax({
-    url: $url,
-    success: function (data) {
-        stocks = data.stocks;
-        for (var i = 0; i < stocks.length; i++) {
-            var stock = stocks[i];
-            var $clone = $("table#portfolio").find('tr.hide.sample').clone(true).removeClass("hide")
-            $clone.find("td.company").text(stock.name)
-            $clone.find("td.symbol").text(stock.ticker)
-            $clone.find("td.sector").text(stock.sector)
-            $clone.find("td.quantity").text(stock.quantity)
-            $clone.find("td.last_price").text(stock.price)
-            $clone.find("td.market_value").text(stock.mkt_value)
-            $("table#portfolio").append($clone)
-        }
-    }
-});
+stocks = user_portfolio.stocks;
+for (var i = 0; i < stocks.length; i++) {
+    var stock = stocks[i];
+    var $clone = $("table#portfolio").find('tr.hide.sample').clone(true).removeClass("hide")
+    $clone.find("td.company").text(stock.name)
+    $clone.find("td.symbol").text(stock.ticker)
+    $clone.find("td.sector").text(stock.sector)
+    $clone.find("td.quantity").text(stock.quantity)
+    $clone.find("td.last_price").text(stock.price)
+    $clone.find("td.market_value").text(stock.mkt_value)
+    $("table#portfolio").append($clone)
+}
