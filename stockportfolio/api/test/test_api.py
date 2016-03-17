fa49584652a2def6b5bb5a9c92fb27c0949caa75
Rohan Kapoor <rohan@rohankapoor.com>
2016-03-17 02:52:35 -0500

    Writing a unit test for the ApiTestCase

--- stockportfolio/api/test/test_api.py
+++ stockportfolio/api/test/test_api.py
@@ -1,12 +1,28 @@
-import unittest
-import stockportfolio.api.api as api
-from django.conf.urls import url
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from stockportfolio.api.models import Portfolio
+from stockportfolio.api import api
+import json
 
-class TestAPI(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.user = 'thibaut.xiong@gmail.com'
+class ApiTestCase(TestCase):
 
-    # def test_add_stock(self):
-        url(r'^portfolio/(?P<portfolio_id>\d+)/addstock$', api.add_stock, name="add_stock"),
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='test', email='test@test.com', password='joking')
+        self.portfolio = Portfolio.objects.create(portfolio_user=self.user)
+        self.portfolio.save()
+
+    def test_get_portfolio_by_user(self):
+        request = self.factory.get(
+            reverse('get_portfolio_by_user', kwargs={'user_id': self.user.id}))
+        request.user = self.user
+        response = api.get_portfolio_by_user(request, self.user.id)
+        self.assertEqual(response.status_code, 200)
+        expected_content = json.loads('{"risk_history": [], "portfolio_id": 1, "sector_allocations": {}, "date_created": "2016-03-17 02:35:55.273000", "stocks": [], "portfolio_userid": 1}')
+        expected_content.pop('date_created', None)
+        received_content = json.loads(response.content)
+        received_content.pop('date_created', None)
+        self.assertEqual(expected_content, received_content)
