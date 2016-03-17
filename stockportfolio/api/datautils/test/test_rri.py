6059b5d8cd4a201c98a6980d76190511d3d6ef45
Rohan Kapoor <rohan@rohankapoor.com>
2016-03-17 00:45:18 -0500

    rkapoor6 sgupta40 refactoring rri tests to work within the DJango environment

--- stockportfolio/api/datautils/test/test_rri.py
+++ stockportfolio/api/datautils/test/test_rri.py
@@ -1,6 +1,8 @@
 import unittest
 from datetime import datetime as dt
 from stockportfolio.api.datautils.rri import *
+from mock import Mock
+import math
 
 """
 Test methods in rri.py
@@ -13,21 +15,37 @@ class TestRRI(unittest.TestCase):
 
     def test_compute_portfolio_rri_for_range1(self):
         """ Tests the compute_portfolio_rri_for_range function """
-        stock_list = ["AAPL", "NFLX", "FB"]
-        quantity_list = [10, 10, 10]
+        apple = Mock()
+        apple.stock_ticker = 'AAPL'
+        apple.stock_quantity = 10
+        netflix = Mock()
+        netflix.stock_ticker = 'NFLX'
+        netflix.stock_quantity = 10
+        fb = Mock()
+        fb.stock_ticker = 'FB'
+        fb.stock_quantity = 10
+        stocks = [apple, netflix, fb]
         start_date = "03/03/2016"
         end_date = "03/13/2016"
-        rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
-        self.assertEqual(rri, 1.48118245233)
+        rri = compute_portfolio_rri_for_range(stocks, start_date, end_date)
+        self.assertAlmostEqual(rri, 1.4811878169633279, places=3)
 
     def test_compute_portfolio_rri_for_range2(self):
         """ Tests the compute_portfolio_rri_for_range function when quantity is zero"""
-        stock_list = ["AAPL", "NFLX", "FB"]
-        quantity_list = [0, 0, 0]
+        apple = Mock()
+        apple.stock_ticker = 'AAPL'
+        apple.stock_quantity = 0
+        netflix = Mock()
+        netflix.stock_ticker = 'NFLX'
+        netflix.stock_quantity = 0
+        fb = Mock()
+        fb.stock_ticker = 'FB'
+        fb.stock_quantity = 0
+        stocks = [apple, netflix, fb]
         start_date = "03/03/2016"
         end_date = "03/13/2016"
-        rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
-        self.assertEqual(rri, nan) #TODO: <type 'numpy.float64'> mai nan kaise likhe?
+        rri = compute_portfolio_rri_for_range(stocks, start_date, end_date)
+        self.assertTrue(math.isnan(rri))
 
     def test_compute_stock_rri_for_range1(self):
         """ Tests the compute_stock_rri_for_range function """
@@ -35,7 +53,7 @@ class TestRRI(unittest.TestCase):
         start_date = "03/03/2016"
         end_date = "03/13/2016"
         rri = compute_stock_rri_for_range(symbol, start_date, end_date)
-        self.assertEqual(rri, 1.73629103182)
+        self.assertAlmostEqual(rri, 1.73629103182, places=3)
 
     def test_compute_stock_rri_for_range2(self):
         """ Tests the compute_stock_rri_for_range function """
@@ -47,10 +65,16 @@ class TestRRI(unittest.TestCase):
 
     def test_compute_portfolio_rri_validity(self):
         """ Tests the compute_portfolio_rri_for_range function """
-        stock_list = ["AAPL", "NFLX", "FB"]
-        quantity_list = [10, 0, 0]
-        start_date = "03/03/2016"
-        end_date = "03/13/2016"
+        apple = Mock()
+        apple.stock_ticker = 'AAPL'
+        apple.stock_quantity = 10
+        netflix = Mock()
+        netflix.stock_ticker = 'NFLX'
+        netflix.stock_quantity = 10
+        fb = Mock()
+        fb.stock_ticker = 'FB'
+        fb.stock_quantity = 10
+        stocks = [apple, netflix, fb]
         p_rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
-        s_rri = compute_stock_rri_for_range(symbol, start_date, end_date)
+        s_rri = compute_stock_rri_for_range(apple.stock_ticker, start_date, end_date)
         self.assertEqual(p_rri, s_rri)
