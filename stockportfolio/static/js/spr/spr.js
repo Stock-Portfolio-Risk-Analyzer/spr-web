c152f32e1e5a2b52c240a42ade8afa1d9c353bda
gleisner <ahashle2@illinois.edu>
2016-04-09 21:52:44 -0500

    front end now pulls from back end

--- stockportfolio/static/js/spr/spr.js
+++ stockportfolio/static/js/spr/spr.js
@@ -1,27 +1,55 @@
 window.onload = function sprInit() {
-    attachEvents()
-
+    attachEvents() 
 }
 
 function attachEvents() {
     // for some reason, addEventListener will only attach an event listener 
     // to the first element in a loop. that's the reason for this monstrosity 
-    document.getElementById("stableModal").onclick=updateStockRec("stable")
-    document.getElementById("highModal").onclick=updateStockRec("high")
-    document.getElementById("lowModal").onclick=updateStockRec("low")
-    document.getElementById("diverseModal").onclick=updateStockRec("diverse")
+    document.getElementById("stableModal").onclick=getRecs("stable")
+    document.getElementById("highModal").onclick=getRecs("high")
+    document.getElementById("lowModal").onclick=getRecs("low")
+    document.getElementById("diverseModal").onclick=getRecs("diverse")
     
 }
 
-function updateStockRec(recType) {
-    var json = undefined // TODO: pull this from back-end
+function getRecs(recType) {
+    var portfolios = document.querySelectorAll('[data-portfolioid]');
+    var curr;
+    for (i=0; i < portfolios.length; i++) {
+        var id = portfolios[i]
+        if (id.className.indexOf("current-page") > -1) {
+            curr = id.getAttribute("data-portfolioid")
+            break
+        }
+    }
+    var request = new XMLHttpRequest()
+    request.rec = recType
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            updateStockRec(this.responseText, this.rec)
+        }
+    }
+    request.open("GET", 
+                 window.location.protocol+"//"+window.location.host+
+                 "/api/portfolio/"+curr+"/stock_rec")
+    request.send()
+}
+
+function updateStockRec(json, recType) {
+    var recs = JSON.parse(json)
     var modalBody = document.getElementById(recType+"-rec-body")
     var list  = document.createElement("ul")
     list.className += "list-unstyled top_profiles scroll-view"
-    var stocks = json[recType]
-    for(i=0;i<stocks.length;i++){
-        item = stockElement(stocks[i])
+    var stocks = recs[recType]
+    if (stocks === undefined) {
+        var item = newElem("p", "", "")
+        item.innerHTML = "No recommendations right now. Check again later!"
         list.appendChild(item)
+    } else {
+        for(i=0;i<stocks.length;i++){
+            var item = stockElement(stocks[i])
+            list.appendChild(item)
+        }
     }
     modalBody.appendChild(list) 
 }
