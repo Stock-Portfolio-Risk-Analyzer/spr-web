e531eae44e81a42141cec8537e638a2bbb1f1b2c
Arpit Mathur <arpitdevmathur@gmail.com>
2016-04-12 17:35:07 -0500

    Revert "refactored portfolio_table.js"
    This reverts commit 56c9525521b568493b6460c0a5cc57cbe78d303f.

--- stockportfolio/static/js/spr/diversity_charts.js
+++ stockportfolio/static/js/spr/diversity_charts.js
@@ -61,7 +61,7 @@ function loadDiversityGraphs() {
     for (var i = 0; i < numOfSectors; i++) {
 
         valueOfSector = valueOfSectors[namesOfSectors[i]]
-        
+
         var newStock = {
             value: valueOfSector / totalValueOfStocks,
             color: arrayOfColors[i]
@@ -111,6 +111,30 @@ function loadDiversityGraphs() {
         risk_div.innerHTML = "N/A"
     }
 
+//Make Portfolio Table dynamic
+    //remove all elements on reload, except hidden sample row.
+    var $clone = $("table#portfolio").find('tr.hide.sample').remove().clone();
+    console.log($clone)
+    $("table#portfolio tbody tr").remove()
+    $("table#portfolio").append($clone)
+
+    for (var i = 0; i < stocks.length; i++) {
+        var stock = stocks[i];
+        var $clone = $("table#portfolio").find('tr.hide.sample').clone(true).removeClass("hide")
+        $clone.find("td.company").text(stock.name)
+        $clone.find("td.symbol").text(stock.ticker)
+        $clone.find("td.sector").text(stock.sector)
+        $clone.find("td.quantity").text(stock.quantity)
+        $clone.find("td.last_price").text(stock.price)
+        $clone.find("td.market_value").text(stock.mkt_value)
+        $("table#portfolio").append($clone)
+    }
+    //Load Rank
+    if(user_portfolio.rank != null){
+        $("#risk_rank").html(user_portfolio.rank)
+    } else {
+        $("#risk_rank").html("N/A")
+    }
 }
 
 
