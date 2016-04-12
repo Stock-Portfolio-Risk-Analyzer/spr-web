ee659e811974db515417eee7b327294ffaf5ddab
Thibaut Xiong <txiong3@illinois.edu>
2016-04-12 18:38:32 -0500

    refactor: docstrings, etc

--- stockportfolio/api/datautils/rri.py
+++ stockportfolio/api/datautils/rri.py
@@ -10,15 +10,13 @@ Author - Shivam Gupta (sgupta40@illinois.edu)
 """
 
 
-def compute_daily_change_for_past_given_days(symbol, number_of_days_back):
+def compute_daily_change_for_past_given_days(symbol, n_days_back=1):
     """
-    Parameter:  symbol -> ticker symbol of the stock (Type -> String)
-            number_of_days_back -> number of days back from today
-                        for which you want the daily change
-                        (Type -> integer)
-    return: list of daily change (Type -> list float)
+    Parameter:  symbol (str):      ticker symbol of the stock (Type -> String)
+                n_days_back (int): number of days back from today for which you want the daily change
+    return: list of daily change (list(float))
     """
-    start_date = date.today() - timedelta(days=number_of_days_back)
+    start_date = date.today() - timedelta(days=n_days_back)
     end_date = date.today()
     symbol_data = yahoo_finance.get_stock_data(symbol, start_date, end_date)
     return list(symbol_data['Close'].pct_change()*100)[1:]
@@ -26,10 +24,10 @@ def compute_daily_change_for_past_given_days(symbol, number_of_days_back):
 
 def compute_daily_change_for_range(symbol, start_date, end_date):
     """
-    Parameter:  symbol -> ticker symbol of the stock (Type -> String)
-            start_date, end_date -> range you want to compute on (Type -> String)
+    Parameter:  symbol (str):               ticker symbol of the stock (Type -> String)
+                start_date, end_date (str): range you want to compute on (Type -> String)
 
-    return: list of daily change (Type -> list float)
+    return: list of daily change (list(float))
     """
     symbol_data = yahoo_finance.get_stock_data(symbol, start_date, end_date)
     return list(symbol_data['Close'].pct_change()*100)[1:]
@@ -37,33 +35,30 @@ def compute_daily_change_for_range(symbol, start_date, end_date):
 
 def compute_covariance(a, b):
     """
-    Computes covariance
-    Parameter: Two lists of integers/floats
+    Computes covariance.
+    Parameter: a, b (list(float))
     Return: float
     """
     return np.cov(np.array([a, b]))[0, 1]
 
+
 def compute_variance(a):
     """
     Computes Variance
-    Parameter: List of integers/floats
-    Return: float
+    Parameter: a (float):
+    Return: (float)
     """
     return np.var(a)
 
 
-def compute_stock_rri_for_today(symbol, number_of_days_back):
+def compute_stock_rri_for_today(symbol, n_days_back=1):
     """
-    Parameter:  symbol -> ticker symbol of the stock (Type -> String)
-            number_of_days_back ->  number of days back from today
-                        for which you want rri
-                        (Type -> integer)
-
+    Parameter:  symbol (str):      ticker symbol of the stock
+                n_days_back (int): number of days back from today for which you want rri
     return: float
     """
-    stock_daily_change  = compute_daily_change_for_past_given_days(symbol, number_of_days_back)
-    index_daily_change  = compute_daily_change_for_past_given_days("NYA", number_of_days_back)
-
+    stock_daily_change  = compute_daily_change_for_past_given_days(symbol, n_days_back)
+    index_daily_change  = compute_daily_change_for_past_given_days("NYA", n_days_back)
     covariance_val = compute_covariance(stock_daily_change, index_daily_change)
     variance_val   = compute_variance(index_daily_change)
     rri = covariance_val/variance_val
@@ -72,9 +67,9 @@ def compute_stock_rri_for_today(symbol, number_of_days_back):
 
 def compute_stock_rri_for_range(symbol, start_date, end_date):
     """
-    Parameter:  symbol -> ticker symbol of the stock (Type -> String)
-            start_date, end_date -> range you want to compute rri on (Type -> String)
-    return: float
+    Parameter:  symbol (str):               ticker symbol of the stock
+                start_date, end_date (str): range you want to compute rri on
+    return: (float)
     """
     stock_daily_change  = compute_daily_change_for_range(symbol, start_date, end_date)
     index_daily_change  = compute_daily_change_for_range("NYA", start_date, end_date)
@@ -85,21 +80,19 @@ def compute_stock_rri_for_range(symbol, start_date, end_date):
     return (rri + 1)
 
 
-def compute_portfolio_rri_for_today(stocks, number_of_days_back):
+def compute_portfolio_rri_for_today(stocks, n_days_back=1):
     """
-    Computes RRI for a portfolio
-    Parameter:  stocks -> list of stock objects
-            number_of_days_back ->  number of days back from today
-                        for which you want rri
-                        (Type -> integer)
-    Return: float
+    Computes RRI for a portfolio.
+    Parameter:  stocks (list(Stock)): list of stock objects
+                n_days_back (int):    number of days back from today for which you want rri
+    Return: (float)
     """
     total_rri = 0.0
     total_quantity = 0
     for stock in stocks:
         ticker = stock.stock_ticker
         quantity = stock.stock_quantity
-        stock_rri = compute_stock_rri_for_today(ticker, number_of_days_back)
+        stock_rri = compute_stock_rri_for_today(ticker, n_days_back)
         total_rri = total_rri + (stock_rri * quantity)
         total_quantity = total_quantity + quantity
 
@@ -110,9 +103,9 @@ def compute_portfolio_rri_for_today(stocks, number_of_days_back):
 def compute_portfolio_rri_for_range(stocks, start_date, end_date):
     """
     Computes RRI for a portfolio
-    Parameter:  stocks -> list of stock objects
-            start_date, end_date -> range you want to compute rri on (Type -> String)
-    Return: float
+    Parameter:  stocks (list(Stock)):       list of stock objects
+                start_date, end_date (str): range you want to compute rri on
+    Return: (float)
     """
     total_rri = 0.0
     total_quantity = 0
@@ -122,7 +115,4 @@ def compute_portfolio_rri_for_range(stocks, start_date, end_date):
         stock_rri = compute_stock_rri_for_range(ticker, start_date, end_date)
         total_rri = total_rri + (stock_rri * quantity)
         total_quantity = total_quantity + quantity
-
-    portfolio_rri = (total_rri / total_quantity)
-
-    return portfolio_rri
+    return (total_rri / total_quantity)
