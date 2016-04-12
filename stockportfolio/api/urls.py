2bf6f3423f0121e24fbfffca98a702519c67704e
gleisner <ahashle2@illinois.edu>
2016-04-12 14:18:15 -0500

    removed test-requirements

--- stockportfolio/api/urls.py
+++ stockportfolio/api/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     url(r'^modify_account/$', views.modify_account, name='modify_account'),
     url(r'^portfolio/(?P<portfolio_id>\d+)/modify$', api.modify_portfolio_form_post, name="modify_portfolio_form_post"),
     url(r'^utils/calculate-rri/$', views.calculate_all_rris, name='utils_calculate_rri'),
-    url(r'^portfolio/(?P<portfolio_id>\d+)/stock_rec$', api.stock_rec, name="stock_rec")
+    url(r'^portfolio/(?P<portfolio_id>\d+)/stock_rec$', api.stock_rec, name="stock_rec"),
     url(r'^(?P<ticker>[A-Z]+)/details', views.stock_interface, name='stock_interface')
 ]
