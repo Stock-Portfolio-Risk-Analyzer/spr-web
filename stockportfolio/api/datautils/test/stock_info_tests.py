a2d08b7fe0cd48a66a351569817a2d09a919015f
Shivam Gupta <shivamvmc@gmail.com>
2016-04-13 14:08:07 -0500

    Update stock_info_tests.py

--- stockportfolio/api/datautils/test/stock_info_tests.py
+++ stockportfolio/api/datautils/test/stock_info_tests.py
@@ -1,53 +1,37 @@
 import unittest
 from datetime import datetime as dt
 from stockportfolio.api.datautils.rri import *
+from stockportfolio.api.datautils.stock_info import *
 from mock import Mock
 import math
 
 """
 Test methods in rri.py
 Author: Shivam Gupta (sgupta40@illinois.edu)
-        Rohan Kapoor (rkapoor6@illinois.edu)
+        Laurynas Tamulevičius
 """
 
 
-class TestRRI(unittest.TestCase):
+class TestStockInfo(unittest.TestCase):
 
-    def test_compute_portfolio_rri_for_range1(self):
-        """ Tests the compute_portfolio_rri_for_range function """
+    def test_get_company_industry(self):
+        """ Tests the get_company_industry function """
         apple = Mock()
         apple.stock_ticker = 'AAPL'
-        apple.stock_quantity = 10
-        netflix = Mock()
-        netflix.stock_ticker = 'NFLX'
-        netflix.stock_quantity = 10
-        fb = Mock()
-        fb.stock_ticker = 'FB'
-        fb.stock_quantity = 10
-        stocks = [apple, netflix, fb]
-        start_date = "03/03/2016"
-        end_date = "03/13/2016"
-        rri = compute_portfolio_rri_for_range(stocks, start_date, end_date)
-        self.assertAlmostEqual(rri, 1.4811878169633279, places=3)
+        industry = get_company_industry(apple.stock_ticker)
+        self.assertAlmostEqual(industry, "Electronic Equipment")
 
-    def test_compute_portfolio_rri_for_range2(self):
-        """ Tests the compute_portfolio_rri_for_range function when quantity is zero"""
-        apple = Mock()
-        apple.stock_ticker = 'AAPL'
-        apple.stock_quantity = 0
+    def test_get_company_rri_for_today(self):
+        """ Tests the get_company_rri_for_today function """
         netflix = Mock()
         netflix.stock_ticker = 'NFLX'
         netflix.stock_quantity = 0
-        fb = Mock()
-        fb.stock_ticker = 'FB'
-        fb.stock_quantity = 0
-        stocks = [apple, netflix, fb]
-        start_date = "03/03/2016"
-        end_date = "03/13/2016"
-        rri = compute_portfolio_rri_for_range(stocks, start_date, end_date)
-        self.assertTrue(math.isnan(rri))
+        number_of_days_back = 4
+        pre_rri  = compute_stock_rri_for_today(netflix.stock_ticker, number_of_days_back)
+        this_rri = get_company_rri_for_today(netflix.stock_ticker, number_of_days_back)
+        self.assertTrue(pre_rri, this_rri)
 
-    def test_compute_stock_rri_for_range1(self):
+    def test_compute_stock_rri_for_range(self):
         """ Tests the compute_stock_rri_for_range function """
         symbol = "AAPL"
         start_date = "03/03/2016"
@@ -55,10 +39,16 @@ class TestRRI(unittest.TestCase):
         rri = compute_stock_rri_for_range(symbol, start_date, end_date)
         self.assertAlmostEqual(rri, 1.73629103182, places=3)
 
-    def test_compute_stock_rri_for_range2(self):
-        """ Tests the compute_stock_rri_for_range function """
-        symbol = "GDDY"
-        start_date = "03/03/2016"
-        end_date = "03/13/2016"
-        rri = compute_stock_rri_for_range(symbol, start_date, end_date)
-        self.assertAlmostEqual(rri, 2.2967252609958937, places=3)
+    def test_get_company_name(self):
+        """ Tests the get_company_name function """
+        fb = Mock()
+        fb.stock_ticker = 'FB'
+        industry = get_company_name(fb.stock_ticker)
+        self.assertTrue(industry == "Facebook, Inc.")
+        
+    def test_get_company_sector(self):
+        """ Tests the get_company_sector function """
+        google = Mock()
+        google.stock_ticker = 'GOOG'
+        industry = get_company_sector(google.stock_ticker)
+        self.assertTrue(industry == "Technology")
