492e2d9efa86d4edefbedc3c4e5ad08160956310
Rohan Kapoor <rohan@rohankapoor.com>
2016-04-18 13:59:17 -0500

    Move stock_quantity to a separate model so that we stop storing duplicate stocks in our database. (Yes this removes the duplicates too)

--- stockportfolio/api/models.py
+++ stockportfolio/api/models.py
@@ -3,21 +3,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Stock(models.Model):
-    """
-
-    """
-    stock_id = models.AutoField(primary_key=True)
-    stock_quantity = models.IntegerField(default=0)
-    stock_ticker = models.CharField(max_length=8, default="")
-    stock_name = models.CharField(max_length=200)
-    stock_beta = models.FloatField(default=0.0)
-    stock_sector = models.CharField(max_length=200, default="Other")
-
-    def __str__(self):
-        return '{} {} {}'.format(self.stock_id, self.stock_ticker, self.stock_beta)
-
-
 class Risk(models.Model):
     """
 
@@ -30,15 +15,40 @@ class Risk(models.Model):
         return '{} @ {}'.format(self.risk_value, self.risk_date)
 
 
+class Stock(models.Model):
+    """
+
+    """
+    stock_id = models.AutoField(primary_key=True)
+    stock_ticker = models.CharField(max_length=8, default="")
+    stock_name = models.CharField(max_length=200)
+    stock_sector = models.CharField(max_length=200, default="Other")
+    stock_risk = models.ManyToManyField(Risk)
+
+    def __str__(self):
+        return '{} {}'.format(self.stock_id, self.stock_ticker)
+
+
+class StockPortfolio(models.Model):
+    """
+
+    """
+    stock = models.ForeignKey(Stock)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.stock.stock_ticker)
+
+
 class Portfolio(models.Model):
     """
 
     """
     portfolio_id = models.AutoField(primary_key=True)
     portfolio_name = models.CharField(max_length=50, null=True, blank=True)
-    portfolio_stocks = models.ManyToManyField(Stock)
     portfolio_user = models.ForeignKey(User)
     portfolio_risk = models.ManyToManyField(Risk)
+    portfolio_stocks = models.ManyToManyField(StockPortfolio)
 
     def __str__(self):
         return self.portfolio_name if self.portfolio_name else "Unnamed"
