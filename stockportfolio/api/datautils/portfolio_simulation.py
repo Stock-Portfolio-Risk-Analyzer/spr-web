0878e4570cf788f22f6541c6eaccb0180b55932d
Thibaut Xiong <txiong3@illinois.edu>
2016-04-23 18:06:14 -0500

    timeseries for simulated portfolio values/returns + tests

---
+++ stockportfolio/api/datautils/portfolio_simulation.py
@@ -0,0 +1,86 @@
+import scipy.stats
+import pandas as pd
+import datetime as dt
+from yahoo_finance import get_stock_data
+
+def get_portfolio_returns_series(portfolio, start_date=None, end_date=None, price_field='Adj Close'):
+    """
+    Simulates portfolio returns assuming the portfolio was bought on start_date and held through end_date
+    :param portfolio:
+    :param start_date:
+    :param end_date:
+    :param price_field:
+    :return:
+    """
+    if start_date is None:
+        start_date = dt.datetime(year=2008, month=1, day=1)
+    if end_date is None:
+        end_date = dt.datetime.today()
+
+    portfolio_value_series = get_portfolio_value_series(portfolio,
+                                                        start_date=start_date,
+                                                        end_date=end_date,
+                                                        price_field=price_field)
+
+
+    portfolio_returns_series = portfolio_value_series.pct_change().dropna()
+    return portfolio_returns_series
+
+def get_portfolio_value_series(portfolio, start_date=None, end_date=None, price_field='Adj Close'):
+    """
+    Generate a time-series of the portfolio assuming the portfolio was bought on start_date and held through end_date.
+    :param portfolio: (dict) symbol:quantity
+    :param start_date: (DateTime)
+    :param end_date: (DateTime)
+    :param price_field: (str) default: 'Adj Close'
+    :return:
+    """
+    if start_date is None:
+        start_date = dt.datetime(year=2008, month=1, day=1)
+    if end_date is None:
+        end_date = dt.datetime.today()
+
+    position_values = []
+    date_range = pd.date_range(start_date, end_date)
+    for symbol, quantity in portfolio.items():
+        position_value_series = get_position_value_series(symbol, quantity, start_date, end_date, price_field)
+        position_values.append(position_value_series)
+
+    portfolio_value_series = pd.Series(data=0, index=date_range)
+    for symbol_value_series in position_values:
+        portfolio_value_series = portfolio_value_series+symbol_value_series
+
+    portfolio_value_series = portfolio_value_series.dropna()
+    return portfolio_value_series
+
+def get_position_value_series(symbol, quantity, start_date=None, end_date=None, price_field='Adj Close'):
+    """
+    Generate a time-series of a single position's value assuming it was bought on start_date and held through end_date.
+    :param symbol: (str)
+    :param quantity: (float)
+    :param start_date: (DateTime)
+    :param end_date: (DateTime)
+    :param price_field: (str)
+    :return: (pd.Series)
+    """
+
+    if start_date is None:
+        start_date = dt.datetime(year=2008, month=1, day=1)
+    if end_date is None:
+        end_date = dt.datetime.today()
+
+    price_series = get_stock_data(symbol, start_date, end_date)[price_field]
+    holdings = pd.Series(data=quantity, index=pd.date_range(start_date, end_date))
+    position_value_series = (price_series*holdings).dropna()
+    return position_value_series
+
+def calculate_alpha_beta(returns, benchmark_returns=None):
+    """
+    Calculates alpha and beta.
+    :param returns:
+    :param benchmark_returns: (
+    :return: (float) alpha, (float) beta
+    """
+    ret_index = returns.index
+    beta, alpha = scipy.stats.linregress(benchmark_returns.loc[ret_index].values, returns.values)[:2]
+    return alpha * 30, beta
---
+++ stockportfolio/api/datautils/test/test_portfolio_simulation.py
@@ -0,0 +1,94 @@
+import unittest
+import pandas as pd
+import datetime as dt
+import stockportfolio.api.datautils.yahoo_finance as yf
+import stockportfolio.api.datautils.portfolio_simulation as ps
+
+class TestPortfolioSimulation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.start_date = dt.datetime(year=2008, month=1, day=1)
+        cls.end_date = dt.datetime(year=2016, month=4, day=20)
+        cls.dates = pd.date_range(cls.start_date, cls.end_date)
+
+        cls.portfolio = {
+            'AAPL': 5,
+            'MSFT': 10,
+            'ORCL': 15
+        }
+
+        cls.price_field = 'Adj Close'
+        cls.test_symbol = 'IBM'
+        cls.test_qty = 100
+
+    def test_get_portfolio_returns_series(self):
+        portfolio_returns_series = ps.get_portfolio_returns_series(self.portfolio,
+                                                                   start_date=self.start_date,
+                                                                   end_date=self.end_date,
+                                                                   price_field=self.price_field)
+
+        for day in self.dates:
+            try:
+                total_portfolio_value_prev_day = 0
+                total_portfolio_value_curr_day = 0
+                for symbol, quantity in self.portfolio.items():
+                    prev_day = day.to_datetime()-dt.timedelta(days=1)
+                    prev_price = yf.get_stock_data(symbol, prev_day, prev_day)[self.price_field][prev_day]
+                    prev_position_value = prev_price*quantity
+                    total_portfolio_value_prev_day += prev_position_value
+
+                    curr_price = yf.get_stock_data(symbol, day, day)[self.price_field][day]
+                    curr_position_value = curr_price*quantity
+                    total_portfolio_value_curr_day += curr_position_value
+
+                change = total_portfolio_value_curr_day-total_portfolio_value_prev_day
+                pct_change = change/total_portfolio_value_prev_day
+                self.assertEqual(pct_change, portfolio_returns_series[day])
+
+            except Exception as e:
+                # no data for this day (i.e. not a trading day)
+                pass
+
+    def test_get_portfolio_value_series(self):
+        portfolio_value_series = ps.get_portfolio_value_series(self.portfolio,
+                                                               start_date=self.start_date,
+                                                               end_date=self.end_date,
+                                                               price_field=self.price_field)
+
+        for day in self.dates:
+            try:
+                total_portfolio_value = 0
+                for symbol, quantity in self.portfolio.items():
+                    price = yf.get_stock_data(symbol, day.to_datetime(), day.to_datetime())
+                    price = price[self.price_field][day]
+                    position_value = price*quantity
+                    total_portfolio_value += position_value
+                self.assertEqual(portfolio_value_series[day], total_portfolio_value)
+            except Exception as e:
+                # no data for this day (i.e. not a trading day)
+                pass
+
+    def test_get_position_value_series(self):
+        position_value_series = ps.get_position_value_series(self.test_symbol, self.test_qty,
+                                                             start_date=self.start_date,
+                                                             end_date=self.end_date,
+                                                             price_field=self.price_field)
+        for day in self.dates:
+            try:
+                price = yf.get_stock_data(self.test_symbol, day.to_datetime(), day.to_datetime())
+                price = price[self.price_field][day]
+                position_value = price*self.test_qty
+                self.assertEqual(position_value_series[day], position_value)
+            except Exception as e:
+                # no data for this day (i.e. not a trading day)
+                pass
+
+    def test_simulate_returns(self):
+        returns = ps.get_portfolio_returns_series(self.portfolio)
+
+    def calculate_alpha_beta(self):
+        pass
+
+
