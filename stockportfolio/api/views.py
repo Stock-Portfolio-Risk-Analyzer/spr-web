7a9506dbf3b118511b0efb42c5a95b83cd4eacaa
Rohan Kapoor <rohan@rohankapoor.com>
2016-04-25 18:30:49 -0500

    Present stock_tickers directly as a JSON object for jquery autocomplete, removing the broken string to array parsing

--- stockportfolio/api/views.py
+++ stockportfolio/api/views.py
@@ -32,12 +32,14 @@ def dashboard(request):
 
     stock_tickers = list(Stock.objects.all().values_list("stock_ticker"))
     stock_tickers.extend(Stock.objects.all().values_list("stock_name"))
+    stock_tickers = zip(*stock_tickers)
 
     upload_portfolio_form = PortfolioUploadForm()
     context = {
         "user": request.user, "gravatar": g_url,
         "form": form,
-        "upload_portfolio_form": upload_portfolio_form
+        "upload_portfolio_form": upload_portfolio_form,
+        "stock_tickers": json.dumps(stock_tickers[0]),
     }
     context.update(csrf(request))
     return render_to_response("index.html", context)
