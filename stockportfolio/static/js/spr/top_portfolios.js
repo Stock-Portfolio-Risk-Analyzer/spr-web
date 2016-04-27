b55071120b5366419ac2ff7a1049058172b621e5
Rohan Kapoor <rohan@rohankapoor.com>
2016-04-27 15:44:10 -0500

    Finalizing the top portfolios for displaying the details of a portfolio

--- stockportfolio/static/js/spr/top_portfolios.js
+++ stockportfolio/static/js/spr/top_portfolios.js
@@ -25,13 +25,30 @@ var top_portfolio = function(option){
 		$("#top_portfolio tr").bind("click",function() {
 
         	var selectedPortfolio = $(this).closest("tr").find(".id").text();
-        
+
         	var selectedPortfolioUrl = "/api/top-ten/portfolio/" + selectedPortfolio;
        		 $.ajax({
             	url: selectedPortfolioUrl,
             	type: 'GET',
             	success: function(data) {
-            		console.log(data)
+            		$('#top-portfolio-list-view').addClass('hidden');
+                $('#top-portfolio-detail-view').removeClass('hidden');
+
+                var $clone = $("table#top-portfolio-details-table").find('tr.hide.sample').remove().clone();
+                $("table#top-portfolio-details-table tbody tr").remove()
+                $("table#top-portfolio-details-table").append($clone)
+
+                for (var i = 0; i < data.stocks.length; i++) {
+                    var stock = data.stocks[i];
+                    var $clone = $("table#top-portfolio-details-table").find('tr.hide.sample').clone(true).removeClass("hide")
+                    $clone.find("td.company").text(stock.name)
+                    $clone.find("td.symbol").text(stock.ticker)
+                    $clone.find("td.sector").text(stock.sector)
+                    $clone.find("td.quantity").text(stock.quantity)
+                    $clone.find("td.last_price").text(stock.price)
+                    $clone.find("td.market_value").text(stock.mkt_value)
+                    $("table#top-portfolio-details-table").append($clone)
+                }
             	}
         	});
 		});
@@ -52,8 +69,16 @@ $('#topPortfolios').on('show.bs.modal', function() {
   top_portfolio(0);
 });
 
+$('#topPortfolios').on('hide.bs.modal', function() {
+  $('#top-portfolio-list-view').removeClass('hidden');
+  $('#top-portfolio-detail-view').addClass('hidden');
+});
+
 $('#topPortfolios .btn-primary').on('click', function() {
   top_portfolio($(this).find('input').attr('id'));
 });
 
-
+$('.top-portfolio-back-btn').on('click', function() {
+  $('#top-portfolio-list-view').removeClass('hidden');
+  $('#top-portfolio-detail-view').addClass('hidden');
+});
