5df188f8a887b3f2a2d4f939acddcc7b6dde312c
Laurynas <tamulev2@illinois.edu>
2016-05-02 15:17:10 -0500

    commented sentiment tests

--- stockportfolio/api/datautils/test/test_sentiment.py
+++ stockportfolio/api/datautils/test/test_sentiment.py
@@ -5,7 +5,9 @@ import stockportfolio.api.datautils.sentiment as stm
 
 
 class TestSentiment(unittest.TestCase):
-
+    """
+    Tests if fetching sentiments for single stock works
+    """
     def test_get_stock_sentiment(self):
         rticker = 'AAPL'
         rstm = stm.get_stock_sentiment(rticker)
@@ -13,12 +15,16 @@ class TestSentiment(unittest.TestCase):
         fticker = 'Z134'
         fstm = stm.get_stock_sentiment(fticker)
         self.assertTrue(fstm == -1 or fstm == 1)
-
+    """
+    Tests if fetching sentiments for portfolio works
+    """
     def test_get_sentiment_of_a_portfolio(self):
         p = ['AAPL', 'GOOG', 'MSFT', 'FAST', 'DYII']
         pstm = stm.get_sentiment_of_a_portfolio(p)
         self.assertTrue(pstm != 0)
-
+    """
+    Tests if fetching sentiments for a interval of time works
+    """
     def test_get_stock_sentiment_for_a_range(self):
         symbol = 'AAPL'
         start_date = dt(year=2016, month=1, day=1)
@@ -28,7 +34,9 @@ class TestSentiment(unittest.TestCase):
         end_stm = rstm[-1]
         self.assertTrue(len(rstm) != 0)
         self.assertAlmostEqual(end_stm, 0.029)
-
+    """
+    Tests if fetching average sentiment for single stock works 
+    """
     def test_get_average_stock_sentiment_for_a_range(self):
         symbol = 'AAPL'
         start_date = dt(year=2016, month=1, day=1)
@@ -36,7 +44,9 @@ class TestSentiment(unittest.TestCase):
         astm = stm.get_average_stock_sentiment_for_a_range(
             symbol, start_date, end_date)
         self.assertAlmostEqual(astm, 0.0751612903226)
-
+    """
+    Tests if fetching market sentiment works
+    """
     def test_get_market_sentiment(self):
         mstm = stm.get_market_sentiment()
         self.assertTrue(-1 <= mstm <= 1)
