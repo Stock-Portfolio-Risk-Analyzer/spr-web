bc8c886520571a97a0fedafde1dd8d43bda728c3
Rohan Kapoor <rohan@rohankapoor.com>
2016-05-01 19:42:37 -0500

    Hotfix for update rri code

--- stockportfolio/api/utils.py
+++ stockportfolio/api/utils.py
@@ -11,17 +11,17 @@ from stockportfolio.api.models import (Portfolio, PortfolioRank,
 
 def update_rri_for_all_portfolios():
     for portfolio in Portfolio.objects.all():
-        try:
-            stocks = portfolio.portfolio_stocks.all()
-            if not stocks:
-                continue
-            risk = Risk(
-                risk_value=rri.compute_portfolio_rri_for_today(stocks, 10))
-            risk.save()
-            portfolio.portfolio_risk.add(risk)
-            portfolio.save()
-        except:
+        stocks = portfolio.portfolio_stocks.all()
+        if not stocks:
             continue
+        try:
+            risk_value = rri.compute_portfolio_rri_for_today(stocks, 10)
+        except:
+            risk_value = 0
+        risk = Risk(risk_value=risk_value)
+        risk.save()
+        portfolio.portfolio_risk.add(risk)
+        portfolio.save()
 
 
 def update_rank_for_all_portfolios():
