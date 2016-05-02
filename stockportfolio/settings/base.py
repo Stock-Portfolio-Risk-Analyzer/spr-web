7633a871c566a96e6d0278788136a6e2e71a9a3e
gleisner <ahashle2@illinois.edu>
2016-05-02 18:03:41 -0500

    revert settings change

--- stockportfolio/settings/base.py
+++ stockportfolio/settings/base.py
@@ -143,7 +143,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
     '-s',
-    #'stockportfolio.api.datautils.test.test_yahoo_finance'
 ]
 
 ADVANCED_SETTINGS = {
